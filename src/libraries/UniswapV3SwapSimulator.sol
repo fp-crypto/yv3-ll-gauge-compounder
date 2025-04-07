9f04278809157139e8bd5f7edbd9c320aef0154f
FP <fp_crypt@protonmail.com>
2025-04-07 15:40:11 -0700

    chore: fix linter errors

--- src/libraries/UniswapV3SwapSimulator.sol
+++ src/libraries/UniswapV3SwapSimulator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import {IUniswapV3Pool} from "@uniswap-v3-core/interfaces/IUniswapV3Pool.sol";
 import {IUniswapV3Factory} from "@uniswap-v3-core/interfaces/IUniswapV3Factory.sol";
